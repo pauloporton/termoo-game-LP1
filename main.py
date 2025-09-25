ff936ca14e75ffa1f570cfa931062582bba255e1
artursmlucena <artursmlucena@gmail.com>
2025-09-25 18:09:16 -0300

    Codigo Decomposto.

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ def regras():
 {red('Vermelho')}: A letra não faz parte da palavra.""")
     
 
-def valida_entrada(tentativa, palavras):
+def valida_entrada(palavras):
+    tentativa = input('Digite uma palavra com 5 letras: ').lower()
     while True:
-        if len(tentativa) != 5 or not tentativa.isalpha() or  tentativa in palavras:
-            print(f'ERRO! A palavra deve ter 5 caracteres e ser composta apenas por letras.')
+        if len(tentativa) != 5 or not tentativa.isalpha() or tentativa not in palavras:
+            print(f'ERRO! A palavra deve ter 5 caracteres e ser composta apenas por letras sem acento.')
             tentativa = input('Digite uma palavra com 5 letras: ').lower()
         else:
-            break # A entrada é válida, sai do loop de validação
+            return tentativa
     
 
 def escolhe_palavra():
@@ -58,57 +59,62 @@ def entrada():
     print('============================')
     return resp
 
-print(f"""{frase_estilizada('PYTERMOO')}
--> Criado por Paulo Porto e Arthur Lucena""")
-entrada1 = entrada()
 
-if entrada1 == '1':
-    while True:
-        regras()
-        entrada1 = entrada()
-        if entrada != '1':
-            break
+def main():
+
+    print(f"""{frase_estilizada('PYTERMOO')}
+    -> Criado por Paulo Porto e Arthur Lucena""")
+    entrada1 = entrada()
+
+    if entrada1 == '1':
+        while True:
+            regras()
+            entrada1 = entrada()
+            if entrada != '1':
+                break
 
 
-if entrada1 == '2':
-    print(f'{frase_estilizada('jogo iniciado')}')
-    print('=' * 21)
-    print(formatar_vazio())
-    print('=' * 21)
-
-    # escolha da palavra a ser adivinhada
-    palavra_secreta = escolhe_palavra()
-
-    chances = 0
-    max_tentativas = 6
-    win = False
-
-    while chances < max_tentativas and not win:
-        # Bloco de validação de entrada
-        tentativa = input('Digite uma palavra com 5 letras: ').lower()
-        valida_entrada(tentativa, palavras)
-        
-        # Apresenta o resultado formatado
+    if entrada1 == '2':
+        print(f'{frase_estilizada('jogo iniciado')}')
         print('=' * 21)
-        print(formatar_tentativa(testa_cores_letras(palavra_secreta, tentativa)))
+        print(formatar_vazio())
         print('=' * 21)
 
-        # Verifica a condição de vitória
-        if tentativa == palavra_secreta:
-            win = True    
+        # escolha da palavra a ser adivinhada
+        palavra_secreta = escolhe_palavra()
 
-        chances += 1
+        chances = 0
+        max_tentativas = 6
+        win = False
 
-    if win:
-        print(f'{frase_estilizada('voce venceu')}')    
-        print(f'Você acertou a palavra: "{palavra_secreta}"')
+        while chances < max_tentativas and not win:
+            # Bloco de validação de entrada
+            tentativa = valida_entrada(palavras)
+    
+            
+            # Apresenta o resultado formatado
+            print('=' * 21)
+            print(formatar_tentativa(testa_cores_letras(palavra_secreta, tentativa)))
+            print('=' * 21)
+
+            # Verifica a condição de vitória
+            if tentativa == palavra_secreta:
+                win = True    
+
+            chances += 1
+
+        if win:
+            print(f'{frase_estilizada('voce venceu')}')    
+            print(f'Você acertou a palavra: "{palavra_secreta}"')
+        else:
+            print(f'{frase_estilizada('voce perdeu')}')
+            print(f'A palavra era: "{palavra_secreta}"') 
+
+    elif entrada1 == '3':
+        print(f'{frase_estilizada('fim de jogo')}')
+        print('Encerrando sessão...')
+
     else:
-        print(f'{frase_estilizada('voce perdeu')}')
-        print(f'A palavra era: "{palavra_secreta}"') 
+        print('Por favor, digite uma opção válida.')
 
-elif entrada1 == '3':
-    print(f'{frase_estilizada('fim de jogo')}')
-    print('Encerrando sessão...')
-
-else:
-    print('Por favor, digite uma opção válida.')
+main()
